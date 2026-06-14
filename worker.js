5b533210ff898eeaf40f0cf7c5daf10d4fa39635
SalonPartners <cleanerday@gmail.com>
2026-06-14 12:26:17 +0400

    Initial commit

---
+++ worker.js
@@ -0,0 +1,16 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import manifestJSON from '__STATIC_CONTENT_MANIFEST';
+const assetManifest = JSON.parse(manifestJSON);
+
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      return await getAssetFromKV(
+        { request, waitUntil: ctx.waitUntil.bind(ctx) },
+        { ASSET_NAMESPACE: env.__STATIC_CONTENT, ASSET_MANIFEST: assetManifest }
+      );
+    } catch {
+      return new Response('Not found', { status: 404 });
+    }
+  },
+};
